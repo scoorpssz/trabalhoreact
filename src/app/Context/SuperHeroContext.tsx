26148ef87e0d4b30c006b862f7a204d5e1d124a3
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-17 11:58:16 +0000

    linha 29 29 FUUUUCK

--- src/app/Context/SuperHeroContext.tsx
+++ src/app/Context/SuperHeroContext.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { createContext, useState, useEffect } from "react";
 import heroesList from "../shared/heroesList";
-a
+
 interface Hero {
   id: string;
   name: string;
