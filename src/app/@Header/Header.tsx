888f0c53b7e7a6fa9a6e3294d5840422bf8cec8a
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-18 14:44:36 +0000

    fdas

--- src/app/@Header/Header.tsx
+++ src/app/@Header/Header.tsx
@@ -7,7 +7,7 @@ import "./header.css";
 const Header = (props: { project_name: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined; my_name: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined; }) => {
   return (
     <header className='header'>
-      <h1>{props.project_name}</h1>
+       <h1>{props.project_name}</h1>
       <h5>Desenvolvido por {props.my_name}</h5>
       <nav>
         <Link href="/" className='nav'>Home</Link>
