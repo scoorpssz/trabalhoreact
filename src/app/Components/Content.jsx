92674abf34c362a91431a446fac48e4518539a36
José Cláudio <167354970+scoorpssz@users.noreply.github.com>
2024-12-21 18:27:26 +0000

    a arranjar

--- src/app/Components/Content.jsx
+++ src/app/Components/Content.jsx
@@ -10,16 +10,6 @@ const Content = () => {
   useEffect(() => {
     if (context && context.superHeroes.length > 0) {
       setLoading(false);
-      const { superHeroes, setFavorites } = context;
-
-      if (superHeroes.length >= 3) {
-        const randomFavorites = new Set();
-        while (randomFavorites.size < 3) {
-          const randomHero = superHeroes[Math.floor(Math.random() * superHeroes.length)];
-          randomFavorites.add(randomHero.id);
-        }
-        setFavorites(Array.from(randomFavorites));
-      }
     }
   }, [context]);
 
